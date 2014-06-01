914e532b92e0d5446ea1aea11e8bb35b4b19b833
Atlantweb <taseb@mail.ru>
2014-06-01 09:41:23 +0400

    9-40
    Релиз 1

--- main.php
+++ main.php
@@ -99,17 +99,22 @@ box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.3);
 <div class="drinkheader"></div>
 <div class="drinkform">
 <div class="esheodindiv">
-Привет! <?=$user['fio']; ?>  <a href='?exit=1'>ВЫХОД</a>
-<li class="dropdown open">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<ul class="nav navbar-nav">
+            <li class="active">Привет! <?=$user['fio']; ?> </li></ul>
+
+<ul class="nav navbar-nav navbar-right"><li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Меню <b class="caret"></b></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#">Мой профиль</a></li>
+				<li><a href="#">Мой профиль</a></li>
+                <li><a href="#">Настройки</a></li>
                 <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
+                <li> <a href='?exit=1'>ВЫХОД</a></li>
               </ul>
-            </li>
+            </li></ul>
+			</div>
 
 
 
